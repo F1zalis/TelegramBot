6fcf34dde08fe3533f9320b095fc13659ddf13b1
F1zalis <113424929+F1zalis@users.noreply.github.com>
2022-09-20 20:04:05 +0300

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -1,24 +1,22 @@
-from aiogram.types import Message
-from aiogram.types import message
-from aiogram.types import chat
+from aiogram import types
+from dispatcher import dp
 from aiogram.dispatcher.filters import Command
+from aiogram.types import Message
+import config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.exceptions import Throttled
-from aiogram import bot, types
-import aiogram
-from dispatcher import dp
+import re
+from bot import BotDB
 from db import add
 from db import h
 
-
-
-@dp.message_handler(Command('h'))
-async def history_cmd(message: Message):
-        await message.answer(await h())
-
-@dp.message_handler(commands=['r'])
+@dp.message_handler(commands = "r")
 async def add_cmd(message: types.Message):
-    s = ' '.join(message.text.split(' ')[1:])
+    if(not BotDB.user_exists(message.from_user.id)):
+        BotDB.add_user(message.from_user.id)
+        await bot.send_message(message.from_user.id, "Укажите ваш ник") 
+    else:
+        await bot.send_message(message.from_user.id, "Вы уже зарегистрированы!")
     try:
         await dp.throttle('r', rate=7200)
     except Throttled:
@@ -27,7 +25,11 @@ async def add_cmd(message: types.Message):
         await add(s)
         await message.answer('Запис успішно додано!')
 
+@dp.message_handler(Command('h'))
+async def history_cmd(message: Message):
+        await message.answer(await h())
 
 @dp.message_handler(commands=['start'])
 async def start_join(message):
-    await message.answer("Вітаю! Щоб скористатись ботом і написати свою історію напиши /r зразок: Ваш нік, псевдонім, справжні ім'я, після пишете свою історію, якщо ви хочете відправити картинку, відправте посилання. Пишіть все відразу кулдаун на повідомлення 2:00, так що пишіть все і відразу")
+        await message.answer("Вітаю! Щоб скористатись ботом і написати свою історію напиши /r зразок: Ваш нік, псевдонім, справжні ім'я, після пишете свою історію, якщо ви хочете відправити картинку, відправте посилання. Пишіть все відразу кулдаун на повідомлення 2:00, так що пишіть все і відразу")
+
